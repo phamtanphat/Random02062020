0948248d890d79a3737a4dd2cd0303974b7df392
phatdroid94 <phatdroid94@gmail.com>
2020-06-20 18:27:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/random02062020/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.random02062020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // public , private , protected , default
+        int a = 5;
+        int b = 6;
+        int ketqua = tinhtong(a,b);
+        Log.d("BBB",ketqua + "");
+    }
+    private int tinhtong(int value1 , int value2){
+        return value1 + value2;
+    }
+}
