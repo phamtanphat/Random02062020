0f98465f672acb65f20e7470037f5312af51ee3e
phatdroid94 <phatdroid94@gmail.com>
2020-06-23 19:27:54 +0700

    bai tap ve nha

--- app/src/main/java/com/example/random02062020/MainActivity.java
+++ app/src/main/java/com/example/random02062020/MainActivity.java
@@ -24,42 +24,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // public , private , protected , default
-        // Builtin function
 
-        // 1 : Ham lam tron
-//        double a = 5.49;
-//        long newA = Math.round(a);
-//        Log.d("BBB","Ket qua cua a : " + newA );
-        // 2 : Ham lam tron len
-//        double b = 5.01;
-//        double newB = Math.ceil(b);
-//        Log.d("BBB","Ket qua cua b " + newB);
-        // 3 : Ham lam tron xuong
-//        double c = 5.99;
-//        double newC = Math.floor(c);
-//        Log.d("BBB","Ket qua cua c " + newC);
-        // 4 : Ham so sanh
-//        int a = 5;
-//        int b = 6;
-//        int solonnhat =  Math.max(a , b);
-//        Log.d("BBB","So lon nhat " + solonnhat);
-        // 5 : Ham Random
-        // 0 -> 5
 
-//        double valueRandom = Math.floor(Math.random() * 6);
-//        Log.d("BBB","Gia tri random " + valueRandom);
-
-        // 5 -> 15
-//        Random random = new Random();
-//        int value = random.nextInt(15 - 5 + 1) + 5;
-//        Log.d("BBB",value + "");
-
-//         Task 1 : Validation 2 edittext
-//            + Người dùng phải nhập số min và số max
-//            + Số max không được bé hơn số min (Nếu bé hơn thì so max = somin + 1 )
-//         Task 2 : Xu ly random trong khoang input
-//              + Hiển thị theo dạng : 1 - 2 - 3 -
+//        Tắt chức năng cho view (setEnabled)
+//        Nếu hiển thị addBound enable thì resetBound và random về enabled = false và ngược lại
+//        Task 1 : Xử lý nút addBound
+//            + Xử lý validate cho input
+//            + Add các số từ số min tới số max vào trong mảng(Arraylist)
+//        Task 2 : Xử lý nút resetBound
+//            + Xóa dữ liệu trong mảng
+//            + Xóa dữ liệu trong input
+//        Task 3 : Xử lý nút random
+//            + Xử random cho mảng
+//            + Hiển thị kết quả theo chuỗi như sau "1 - 2 - 3 - 4"
         mapView();
         setListener();
     }
